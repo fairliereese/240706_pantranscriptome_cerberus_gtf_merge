89db0ba392cdf8b4530b1a978e64c86ef4797c6b
fairliereese <fairliek@comcast.net>
2024-07-19 16:31:51 +0200

    ups forgot py file

--- snakemake/refmt_gtf.py
+++ snakemake/refmt_gtf.py
@@ -1,55 +1,68 @@
 import sys
 import pyranges as pr
+import cerberus
 
-def rename_novel_genes(gtf_df, bed, tool='iq'):
+def make_hier_entry(df, how='t'):
     """
-    Rename novel genes using a unified set of novel gene
-    bed coordinates across the different samples
+    kind {'g','t'}
     """
-    if tool == 'iq':
-        nov_df = gtf_df.loc[(gtf_df.Feature=='gene')&\
-                    (gtf_df.gene_id.str.contains('novel_gene'))]
-    l1 = len(nov_df.index)
+    agg_dict = {'min_coord': 'min', 'max_coord': 'max'}
+    t_df = df.copy(deep=True)
+    t_df['min_coord'] = t_df[['Start', 'End']].min(axis=1)
+    t_df['max_coord'] = t_df[['Start', 'End']].max(axis=1)
+    if how == 't':
+        gb_cols = ['Chromosome', 'Strand', 'gene_name',
+                   'gene_id', 'transcript_id', 'transcript_name',
+                   'tss_id', 'tes_id',
+                   'new_transcript_id', 'original_transcript_id',
+                   'original_transcript_name', 'ag1', 'ag2']
+    elif how == 'g':
+        gb_cols = ['Chromosome', 'Strand', 'gene_name',
+                   'gene_id']
+    gb_cols = list(set(gb_cols)&(set(t_df.columns)))
 
-    # merge w/ bed
-    cols = nov_df.columns.tolist()
-    nov_df = pr.PyRanges(nov_df)
-    nov_df = nov_df.join(df,
-                          how='left',
-                          strandedness='same')
-    nov_df = nov_df.df
-    l2 = len(nov_df.index)
-    assert l1 == l2
-    assert len(nov_df.loc[nov_df.Name.isnull()].index) == 0
+    cols = gb_cols + ['min_coord', 'max_coord']
+    t_df = t_df[cols]
+    t_df = t_df.groupby(gb_cols, observed=True).agg(agg_dict).reset_index()
+    t_df.rename({'min_coord': 'Start', 'max_coord': 'End'}, axis=1, inplace=True)
+    if how == 't':
+        t_df['Feature'] = 'transcript'
+    elif how == 'g':
+        t_df['Feature'] = 'gene'
 
-    # get a dictionary to map sample-level novel gene
-    # ids to cross-sample novel gene ids
-    gid_dict = dict([(o,n) for o,n in zip(nov_df.gene_id.tolist(),
-                                          nov_df.Name.tolist())])
+    return t_df
 
-    # now update full gtf
-    novel_gids = nov_df.gene_id.unique().tolist()
-    inds = gtf_df.loc[gtf_df.gene_id.isin(novel_gids)].index
-    gtf_df.loc[inds, 'gene_id'] = gtf_df.loc[inds, 'gene_id'].map(gid_dict)
-
-    return gtf_df
-
-def fmt_iq_gtf(ifile, bed, ofile, tool):
+def fmt_gtf(ifile, bed, ofile, tool):
     """
     Add gene name and transcript name
     columns in a more sensible way.
     """
-    df = pr.read_gtf(ifile).as_df()
 
-    # just grab all the gene ids to be the gene names
-    df['gene_name'] = df['gene_id']
+    df = pr.read_gtf(f).df
 
-    # for transcripts / other transcript-level features do the same but restrict to those feats
-    inds = df.loc[df.Feature != 'gene'].index
-    df.loc[inds, 'transcript_name'] = df.loc[inds, 'transcript_id']
+    # espresso -- add gene entries
+    if tool == 'espresso':
+        l1 = len(df.gene_id.unique().tolist())
 
-    # fix up novel genes by orienting them using the same naming system
-    df = rename_novel_genes(df, bed, tool)
+        # make gene entry
+        g_df = make_hier_entry(df, how='g')
+        l2 = len(g_df.loc[g_df.Feature=='gene'].index)
+        assert l1 == l2
+
+        # concat them and then sort gtf
+        df = pd.concat([df, g_df], axis=0)
+        df = cerberus.sort_gtf(df)
+
+    # iq -- add gene names and transcript names
+    elif tool == 'iq':
+
+        # just grab all the gene ids to be the gene names
+        df['gene_name'] = df['gene_id']
+
+        # for transcripts / other transcript-level features do the
+        # same but restrict to those feats
+        inds = df.loc[df.Feature != 'gene'].index
+        df.loc[inds, 'transcript_name'] = df.loc[inds, 'transcript_id']
 
     # convert + save
     df = pr.PyRanges(df)
@@ -57,15 +70,14 @@ def fmt_iq_gtf(ifile, bed, ofile, tool):
 
 def main():
     if len(sys.argv) != 4:
-        print("Usage: python script_name.py <input_gtf_file> <input_bed_file> <output_gtf_file> <tool>")
+        print("Usage: python script_name.py <input_gtf_file> <output_gtf_file> <tool>")
         sys.exit(1)
 
     input_file = sys.argv[1]
-    bed_file = sys.argv[2]
-    output_file = sys.argv[3]
-    tool = sys.argv[4]
+    output_file = sys.argv[2]
+    tool = sys.argv[3]
 
-    fmt_iq_gtf(input_file, bed_file, output_file, tool,)
+    fmt_gtf(input_file, bed_file, output_file, tool)
 
 if __name__ == "__main__":
     main()
